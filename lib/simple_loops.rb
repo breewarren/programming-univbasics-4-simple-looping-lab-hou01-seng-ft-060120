d5e283615f73bc16f674b31656c88ed2c65056cc
Bree Warren <ambreea.warren@gmail.com>
2020-05-11 16:31:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,11 @@
-# Write your methods here
+def loop_message_five_times (message)
+  puts message
+  puts message
+  puts message
+  puts message
+  puts message
+end
+
+def loop_message_n_times (message, integer)
+  puts message
+end
