c9f5dcb65800feeb6a85e2bf62cd527683ba2b40
Bree Warren <ambreea.warren@gmail.com>
2020-05-11 17:16:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,12 +18,3 @@ def output_array (array)
   puts array [count]
   count += 1
 end
-
-def return_string_array (array)
-  count = 0
-  while count < array.length do
-    array[count] = array[count].to_s
-    count += 1
-  end
-  array
-end
